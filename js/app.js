55598e19bb387f0d27049abd1c6748eb3752ea6b
Taylor Welling <tw3080@gmail.com>
2016-08-08 13:03:30 -0700

    removed Math.round() from calculations; numbers are now formatted with angular's currency filter

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ angular.module('waitStaffApp', []).controller('MainCtrl', function($scope) {
 
     // Initilize variables
     vm.init = function() {
-        /* TODO: I'd like these to be formatted as 0.00? */
         vm.subtotal = 0;
         vm.tipAmount = 0;
         vm.mealTotal = 0;
@@ -20,24 +19,22 @@ angular.module('waitStaffApp', []).controller('MainCtrl', function($scope) {
 
     // Calculates charges for each customer
     vm.getCustomerCharges = function() {
-        /* TODO: How can I keep these as numbers AND round them to two decimal places AND keeping the trailing zeros? Seems like I can only pick two of those. */
         // Calculate subtotal of meal
-        vm.subtotal = Math.round(vm.data.baseMealPrice * 1e2) / 1e2;
+        vm.subtotal = vm.data.baseMealPrice;
         // Calculate tip amount based on subtotal and tip percentage
-        vm.tipAmount = Math.round((vm.subtotal * (vm.data.tipPercent / 100)) * 1e2) / 1e2;
+        vm.tipAmount = vm.subtotal * (vm.data.tipPercent / 100);
         // Calculate total cost of meal, including tax and tip
-        vm.mealTotal = Math.round(((vm.subtotal + (vm.subtotal * (vm.data.taxRate / 100))) + vm.tipAmount) * 1e2) / 1e2;
+        vm.mealTotal = vm.subtotal + (vm.subtotal * (vm.data.taxRate / 100)) + vm.tipAmount;
     };
 
     // Calculates cumulative earnings
     vm.getEarnings = function() {
         // Calculate cumulative total tips
-        // TODO: Why is Math.round not working here?
-        vm.totalTips += Math.round(vm.tipAmount * 1e2) / 1e2;
+        vm.totalTips += vm.tipAmount;
         // Calculate total number of meals
         vm.mealCount++;
         // Calculate average tip per meal
-        vm.avgTip = Math.round((vm.totalTips / vm.mealCount) * 1e2) / 1e2;
+        vm.avgTip = vm.totalTips / vm.mealCount;
     };
 
     // Submit form
